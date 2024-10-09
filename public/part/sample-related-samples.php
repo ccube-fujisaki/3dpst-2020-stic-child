0ebeeb4b0fdd3251f7e263ad355007feb6a9f0e7
ejointjp <mail@e-joint.jp>
2024-10-09 20:47:33 +0900

    2024-10-09 20:47:33

--- public/part/sample-related-samples.php
+++ public/part/sample-related-samples.php
@@ -1,18 +1,28 @@
-<?php // 造形サンプル一覧を表示させる
-if ( !$related_samples = get_field('post-related-sample')[0] ) return;
+<?php
+// 造形サンプル一覧を表示させる
+
+// 'post-related-sample' が存在し、かつ配列であり、0番目の要素が存在することを確認
+$related_samples = get_field('post-related-sample');
+if (!$related_samples || !is_array($related_samples) || empty($related_samples[0])) {
+  return;
+}
+
+// $related_samples が安全に取得できた場合、0番目の要素を使う
+$sample = $related_samples[0];
+
 
 $args = array(
-  'posts_per_page'   => -1,
-  'numberposts'      => -1,
-  'orderby'          => 'post_date',
-  'order'            => 'DESC',
-  'include'          => $related_samples,
-  'post_type'        => 'sample',
-  'post_status'      => [ 'publish', 'draft', 'pending', 'future' ],
+  'posts_per_page' => -1,
+  'numberposts' => -1,
+  'orderby' => 'post_date',
+  'order' => 'DESC',
+  'include' => $related_samples,
+  'post_type' => 'sample',
+  'post_status' => ['publish', 'draft', 'pending', 'future'],
   'suppress_filters' => true
 );
 
-if ( !$myposts = get_posts($args)) return;
+if (!$myposts = get_posts($args)) return;
 ?>
 
 <div class="c-related-sample">
@@ -20,11 +30,12 @@ if ( !$myposts = get_posts($args)) return;
   <div class="c-sample">
     <div class="c-row _xs_">
       <div class="c-col _phone6_ _tablet4_ _pc3_">
-      <?php
-      foreach($myposts as $post) : setup_postdata($post);
-        get_template_part( 'component/article-sample-archive' );
-      endforeach; wp_reset_postdata();
-      ?>
+        <?php
+        foreach ($myposts as $post) : setup_postdata($post);
+          get_template_part('component/article-sample-archive');
+        endforeach;
+        wp_reset_postdata();
+        ?>
       </div>
     </div>
   </div>
--- public/searchform.php
+++ public/searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url(); ?>">
-	<input type="search" class="search-field search-form__input" placeholder="サイト内検索" value="<?php echo $_GET['s']; ?>" name="s">
+	<input type="search" class="search-field search-form__input" placeholder="サイト内検索" value="<?php echo isset($_GET['s']) ? esc_attr($_GET['s']) : ''; ?>" name="s">
 	<button type="submit" class="search-submit search-form__button"><i class="fas fa-search"></i></button>
 </form>
